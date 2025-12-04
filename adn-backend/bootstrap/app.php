01baf2e408a18a0325d830efd55680807bb61deb
Jorge Zazueta <jorgeezp10@gmail.com>
2025-12-03 19:12:55 -0700

    Disable CSRF for API endpoints

--- adn-backend/bootstrap/app.php
+++ adn-backend/bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/mutation',
+            '/stats',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
